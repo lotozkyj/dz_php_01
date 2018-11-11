12e4d6976a6fdfc788331658210b47b2c3644911
Андрей <andrey@MacBook-Pro-PC.local>
2018-11-11 14:49:53 +0200

    Creatre page About us

---
+++ pages/about.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+Welcome to our Site!
+<br> <br>
+On this page we tell you about us!
+<br> <br>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quasi, saepe? Atque et exercitationem iure nam voluptatem. Accusantium, eligendi non!
+</body>
+</html>
