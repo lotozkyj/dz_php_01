b2c37d2ea998d9c8604f9c91fd5c63a05eee28b5
Андрей <andrey@MacBook-Pro-PC.local>
2018-11-11 14:54:41 +0200

    Create header file with navigation

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="/">Home Page</a></li>
+    <li><a href="/?page=about">About us</a></li>
+    <li><a href="/?page=contact">Contact</a></li>
+</ul>
--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 $page = @$_GET['page'];
 
 if($page == 'home'){
+    include 'inc/header.php';
     include 'pages/home.php';
 }elseif($page == 'about'){
+    include 'inc/header.php';
     include 'pages/about.php';
 }else{
+    include 'inc/header.php';
     include 'pages/404.php';
 }
+
 
 ?>
